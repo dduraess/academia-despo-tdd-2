69fd50e9f9645943c140f12ca7087eb0d05fc8c7
27352403809 <27352403809@serpro-1572762>
2019-04-17 12:45:50 -0300

    6o commit

--- camelCase/src/main/java/br/gov/serpro/camelCase/CamelCase.java
+++ camelCase/src/main/java/br/gov/serpro/camelCase/CamelCase.java
@@ -8,13 +8,19 @@ public class CamelCase {
 	public static List<String> converterCamelCase(String original){
 		
 		List<String> palavrasCamelCase = new ArrayList<String>();
-		String convertida;
+		String convertida = original;
 		
 		if(Character.isUpperCase(original.charAt(0))) {
-			convertida = original.toLowerCase();
-		} else {
-			convertida = original;
-		}
+			char originalCharArray[] = original.toCharArray();
+			originalCharArray[0] = Character.toLowerCase(original.charAt(0));
+			convertida = String.copyValueOf(originalCharArray);
+		} 
+		
+//		for (int i = 0; i < original.length(); i++) {
+//			do {
+//				
+//			} while (!Character.isUpperCase(original.charAt(i)));
+//		}
 		
 		palavrasCamelCase.add(convertida);
 		return palavrasCamelCase;
