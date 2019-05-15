109eb2c8ba9868a57e3666ad8079c23d6eb5f6e2
27352403809 <davison.souza@serpro.gov.br>
2019-05-15 10:04:45 -0300

    Teste sacar

--- BancoMockado/banco/src/main/java/br/gov/serpro/banco/Hardware.java
+++ BancoMockado/banco/src/main/java/br/gov/serpro/banco/Hardware.java
@@ -3,7 +3,7 @@ package br.gov.serpro.banco;
 /**
  * Created by 27352403809 on 14/05/19.
  */
-public interface Hadware {
+public interface Hardware {
     String pegarNumeroDaContaCartao();
     void entregarDinheiro();
     void lerEnvelope();
--- BancoMockado/banco/src/test/java/br/gov/serpro/banco/TestarCaixaEletronico.java
+++ BancoMockado/banco/src/test/java/br/gov/serpro/banco/TestarCaixaEletronico.java
@@ -11,10 +11,16 @@ public class TestarCaixaEletronico {
 	
 	
 		private CaixaEletronico cx1;
+		private CaixaEletronico cx2;
+		private HardwareMock mockHwContaExistente;
+		private HardwareMock mockHwContaInexistente;
 
 	@Before
 	public void setUpBefore() {
-		cx1 = new CaixaEletronico();
+		mockHwContaExistente = new HardwareMock("1234-5");
+		mockHwContaInexistente = new HardwareMock("9999-9");
+		cx1 = new CaixaEletronico(mockHwContaExistente, new ServicoRemotoMock());
+		cx2 = new CaixaEletronico(mockHwContaInexistente, new ServicoRemotoMock());
 	}
 
 	@Test
@@ -24,7 +30,7 @@ public class TestarCaixaEletronico {
 
 	@Test
 	public void loginFalhou() {
-		assertEquals("Não foi possível autenticar o usuário", cx1.logar());
+		assertEquals("Não foi possível autenticar o usuário", cx2.logar());
 	}
 
 	@Test
